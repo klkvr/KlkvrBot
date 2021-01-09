8fac040730ff644e90c1edbebf94574341f58b09
klkvr <klkvrr@gmail.com>
2021-01-09 08:05:05 +0300

    .

--- weather_control.py
+++ weather_control.py
@@ -13,7 +13,6 @@ if [current_date.hour, current_date.minute] == [0, 0]:
     bulbs_set_color_temp(1700)
 elif current_date.hour >= 8:
     bulbs_set_color_temp(4000)
-if current_time % 86400
 time_100 = sunset_time - 15 * 60
 time_0 = time_100 - 33 * 60
 if time_0+5*60 >= current_time >= time_0:
