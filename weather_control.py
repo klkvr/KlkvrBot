d15c67650536440adc10c9f4a571f8518663f352
klkvr <klkvrr@gmail.com>
2021-01-09 19:22:32 +0300

    .

--- weather_control.py
+++ weather_control.py
@@ -9,10 +9,12 @@ from datetime import datetime
 sunset_time = get_sunset()
 current_time = int(time.time())
 current_date = datetime.fromtimestamp(current_time)
-if [current_date.hour, current_date.minute] == [0, 0]:
-    bulbs_set_color_temp(1700)
-elif current_date.hour >= 8:
-    bulbs_set_color_temp(4000)
+bulbs_data = get_room_data()
+if bulbs_data['power'] == 'on':
+    if [current_date.hour, current_date.minute] == [0, 0]:
+        bulbs_set_color_temp(1700)
+    elif current_date.hour >= 8:
+        bulbs_set_color_temp(4000)
 time_100 = sunset_time - 15 * 60
 time_0 = time_100 - 33 * 60
 if time_0+5*60 >= current_time >= time_0:
